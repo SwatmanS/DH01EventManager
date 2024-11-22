46eb3c4b09a2755b5cdc7c773ca368e415d04e87
SwatmanS <sam.swatman@northumbria.ac.uk>
2024-11-22 14:06:26 +0000

    Add project files.

---
+++ DH01EventManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DH01EventManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
