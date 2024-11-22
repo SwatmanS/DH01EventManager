3a3ed3d23e6fcd15662208e0df2980f28e36db09
SwatmanS <sam.swatman@northumbria.ac.uk>
2024-11-22 14:23:19 +0000

    Added Sam's Test Page

--- DH01EventManager/SamsTestPage.xaml.cs
+++ DH01EventManager/SamsTestPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DH01EventManager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SamsTestPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SamsTestPage : Window
     {
-        public MainWindow()
+        public SamsTestPage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window x = new SamsTestPage();
-            x.Show();
         }
     }
 }
