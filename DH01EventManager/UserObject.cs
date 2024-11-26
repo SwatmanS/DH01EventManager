6ef56a5d77f3e1b2e8cb5f8ce25205f636d7df39
goatlisp@gmail.com <goatlisp@gmail.com>
2024-11-26 15:37:17 +0000

    comments for why the userObjects class is redundant

---
+++ DH01EventManager/UserObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH01EventManager
+{
+    internal class UserObject
+    {
+        //i feel like user attributes like the username and the password should be stored in the
+        //database (and the password should be hashed) for data security 
+        //i honestly feel like the user class is kinda pointless? there's nothing to really store?
+        //whether a user is be logged in or not would be stored as a global boolean and the
+        //code to change that would be in the login page and there would be a function for
+        // logging out
+    }
+}
