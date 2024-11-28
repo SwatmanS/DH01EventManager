7f1d5d7c769236780bff4c19a47dfab467d2c1bb
goatlisp@gmail.com <goatlisp@gmail.com>
2024-11-28 12:16:06 +0000

    populated LocationObject class

--- DH01EventManager/LocationObject.cs
+++ DH01EventManager/LocationObject.cs
@@ -2,5 +2,37 @@
 {
     public class LocationObject
     {
+        //attributes
+        private int locationID;
+        private string locationName;
+        private string locationType;
+        private int locationCapacity;
+
+        public LocationObject(int locationID, string locationName, string locationType, int locationCapacity)
+        {
+            this.locationID = locationID;
+            this.locationName = locationName;
+            this.locationType = locationType;
+            this.locationCapacity = locationCapacity;
+        } //constructor
+
+        //getters
+
+        public int GetLocationID() { return this.locationID; }
+        public string GetLocationName() {return this.locationName; }  
+
+        public string GetlocationType() { return this.locationType;}
+
+        public int GetLocationCapacity() { return this.locationCapacity;}
+
+        //setters
+
+        public void SetLocationID(int locationID) { this.locationID = locationID; }
+        public void SetLocationName(string locationName) { this.locationName = locationName; }  
+
+        public void SetLocationType(string locationType) { this.locationType = locationType; }
+
+        public void SetLocationCapacity(int locationCapacity) { this.locationCapacity = locationCapacity; }
+
     }
 }
