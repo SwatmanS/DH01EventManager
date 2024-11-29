2b0dfc22393430fdb7093516d55bbffbd0938437
goatlisp@gmail.com <goatlisp@gmail.com>
2024-11-29 16:47:19 +0000

    fixed issue EqOI1

--- DH01EventManager/EquipmentObject.cs
+++ DH01EventManager/EquipmentObject.cs
@@ -8,7 +8,7 @@
         private string equipmentType;
         private int equipmentQuantity;
 
-        public EquipmentObject(string ID, string name, string type, int quantity)
+        public EquipmentObject(int ID, string name, string type, int quantity)
         {
             this.equipmentID = ID;
             this.equipmentName = name; 
@@ -27,7 +27,7 @@
 
 
         //setters
-        public void setEquipmentID(String id) { this.equipmentID = id; }
+        public void setEquipmentID(int id) { this.equipmentID = id; }
         public void setEquipmentName(String name) { this.equipmentName = name; }
         public void setEquipmentType(String type) { this.equipmentType = type; }
         public void setEquipmentQuantity(int quantity) { this.equipmentQuantity = quantity; }
