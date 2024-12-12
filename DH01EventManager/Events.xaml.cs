cba37bfcfb16699e06b546216b1bbd0fb8c654bf
jasmine <goatlisp@gmail.com>
2024-12-12 14:32:07 +0000

    removed unnecessary code

--- DH01EventManager/Events.xaml.cs
+++ DH01EventManager/Events.xaml.cs
@@ -46,26 +46,6 @@ namespace DH01EventManager
                 ListOf.Add(evString);
 
             }
-
-            /*foreach (EventObject ev in events)
-            {
-                temp = ev.toString();
-                foreach (UpcomingEvent ups in upEvent)
-                {
-       
-                    temp = ups.addString(temp);
-                    ListOf.Add(temp);
-
-                }
-            }*/
-
-
-            /*string[] ListOf = ["Event Title: 1st Event\n Event date: 01.01.2001,\n Event starts at 6am and ends at 5pm,\n Event Capacity: 120 people,\n Event Staff: Batrisyia Orked, Irfan Kesuma, Haziq Som, Mia Kiambang,\n Event Location: Family Clinic Seventeen",
-                "Event Title: 2nd Event\n Event date: 02.02.2002,\n Event starts at 7am and ends at 12pm,\n Event Capacity: 60 people,\n Event Staff: a, b, c, d,\n Event Location: Family Clinic Seventeen",
-                "Event Title: 3rd Event\n Event date: 03.03.2003,\n Event starts at 9am and ends at 9pm,\n Event Capacity: 120 people,\n Event Staff: Batrisyia Orked, Irfan Kesuma, Haziq Som, Mia Kiambang,\n Event Location: Family Clinic Seventeen",
-                "Event Title: 4th Event\n Event date: 04.04.2004,\n Event starts at 6am and ends at 5pm,\n Event Capacity: 120 people,\n Event Staff: Batrisyia Orked, Irfan Kesuma, Haziq Som, Mia Kiambang,\n Event Location: Family Clinic Seventeen",
-                "Event Title: 5th Event\n Event date: 05.05.2005,\n Event starts at 6am and ends at 5pm,\n Event Capacity: 120 people,\n Event Staff: Batrisyia Orked, Irfan Kesuma, Haziq Som, Mia Kiambang,\n Event Location: Family Clinic Seventeen",];
-            */
             //makes the canvas height dynamic
             canvas.Height = (ListOf.Count + 1) * 240;
 
