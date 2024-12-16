ac47a8a7caeac0d405623d7cfcbe7b5a19cdbda2
Owen Neale <W22023313@northumbria.ac.uk>
2024-12-16 15:39:53 +0000

    Owen Test Page

--- DH01EventManager/OwenTestPage.xaml.cs
+++ DH01EventManager/OwenTestPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,20 @@ namespace DH01EventManager
              *  Put Tests Below
              * 
              */
+            Debug.WriteLine("Test DBAL-60 get all events test");
+            List<EventObject> l =  DBAbstractionLayer.getAllEvents();
+            foreach (EventObject e in l)
+            {
+                Debug.WriteLine(e.toString());
+            }
+            Debug.WriteLine("Test DBAL-67 get all upcoming events test");
+            List<UpcomingEvent> l2 = DBAbstractionLayer.getUpcomingEvents();
+            foreach (UpcomingEvent u in l2)
+            {
+                EventObject e = u;
+                Debug.WriteLine(e.toString());
+                Debug.WriteLine(u.getEstimatedTurnout());
+            }
         }
     }
 }
